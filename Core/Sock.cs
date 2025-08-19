5d7bbce0a30a4abad8bb1faaaf780e0465c051f6
Yize Miao <tomi26351@gmail.com>
2025-08-19 17:06:46 +0300

    Add sock entity and AddSock pageModel

--- Core/Sock.cs
+++ Core/Sock.cs
@@ -5,7 +5,11 @@ public class Sock
     public int Id { get; set; }
     public string SerialNumber { get; set; }
     public double PricePerPair { get; set; }
-    public int QuantityPerSack{ get; set; }
+    public int QuantityPerSack { get; set; }
     public int SackQuantity { get; set; }
+    public double OverallPrice { get; set; }
     public DateTime dateTime { get; set; }
+
+
+    public Sock() { }
 }
--- Web/Pages/AddSock.cshtml.cs
+++ Web/Pages/AddSock.cshtml.cs
@@ -1,10 +1,13 @@
-namespace Jubebe.Pages;
+namespace Jubebe.Web.Pages;
 
+using Core;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 
 public class AddSockModel : PageModel
 {
-
+    [BindProperty]
+    public Sock Sock { get; set; } = new Sock();
     public void OnGet() { }
 }
