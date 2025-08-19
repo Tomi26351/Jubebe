0703e296755bf56b0c6e67146615fdb4cd24b961
Yize Miao <tomi26351@gmail.com>
2025-08-19 17:21:31 +0300

    adding connection credentials to database file

---
+++ Database/Database.cs
@@ -0,0 +1,10 @@
+﻿namespace Database;
+using Microsoft.Data.SqlClient;
+
+public class Database
+{
+    private SqlConnection connection = new SqlConnection(@"Server=MSI\SQLEXPRESS;Database=Jubebe;Trusted_Connection=True;TrustServerCertificate=True;");
+    public Database() { }
+
+        public SqlConnection Connection { get { return connection; } }
+}
